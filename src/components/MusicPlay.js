618c6dcc522c725926a9acec23d54e93ba26266d
graykick <graykick@naver.com>
2017-04-06 12:14:01 +0900

    Flow text (title, artist switch)
    Flow text (title, artist switch)

---
+++ src/components/MusicPlay.js
@@ -0,0 +1,167 @@
+import React, {Component, PropTypes} from 'react';
+import Progress from './Progress';
+import '../App.css';
+
+const propTypes = {};
+const defaultProps = {};
+class MusicPlay extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            title: "I don't wanna live forever I don't wanna live foreverI don't wanna live forever",
+            album: "default album",
+            artist: "Taylor Swift & ZAYN Taylor Swift & ZAYN Taylor Swift & ZAYN",
+            currentTime: -1,
+            totalTime: 10,
+            overflow: {
+                title: false,
+                artist: false
+            },
+            setOverFlow: false
+        }
+
+        window.onresize = () => {
+            this.setState({setOverFlow: false})
+        }
+
+    }
+
+    setFlow() {
+        console.log("set flow")
+        console.log(this);
+        let titleWidth = document.getElementById("Music-Title").offsetWidth;
+        let artistWidth = document.getElementById("Music-Artist").offsetWidth;
+        let viewPortWidth = window.innerWidth || document.body.clientWidth;
+
+        let titleOverflow;
+        let artistOverflow;
+
+        console.log(titleWidth + ", " + viewPortWidth);
+
+        // 곡 제목의 길이가, 뷰포트 너비보다 길면 flow시킨다.
+        if (titleWidth > viewPortWidth) {
+            titleOverflow = true;
+        } else {
+            titleOverflow = false;
+        }
+        // 음악가의 길이가, 뷰포트 너비보다 길면 flow시킨다.
+        if (artistWidth > viewPortWidth) {
+            artistOverflow = true;
+        } else {
+            artistOverflow = false;
+        }
+
+        // 비동기 메서드
+        this.setState({
+            overflow: {
+                title: titleOverflow,
+                artist: artistOverflow
+            }
+        });
+
+        if (titleOverflow && artistOverflow) {
+            // title과 artist모두 overflow라면 다른 marquee keyframes적용
+            // title 설정
+            document.getElementById("Music-Title").style.paddingRight = "80px";
+            titleWidth = document.getElementById("Music-Title").offsetWidth;
+            document.getElementById("flowTextContainer-Title").style.width = (titleWidth * 2) + "px";
+            document.getElementById("flowText-H1-Title").style.width = (titleWidth * 2) + "px";
+            document.getElementById("flowTextContainer-Title").style.animation = "marqueeTitle 30s linear infinite";
+
+            // artist 설정
+            document.getElementById("Music-Artist").style.paddingRight = "80px";
+            artistWidth = document.getElementById("Music-Artist").offsetWidth;
+            document.getElementById("flowTextContainer-Artist").style.width = (artistWidth * 2) + "px";
+            document.getElementById("flowText-H1-Artist").style.width = (artistWidth * 2) + "px";
+            document.getElementById("flowTextContainer-Artist").style.animation = "marqueeArtist 30s linear infinite";
+        } else if (titleOverflow) {
+            // title만 overflow
+            document.getElementById("Music-Title").style.paddingRight = "80px";
+            titleWidth = document.getElementById("Music-Title").offsetWidth;
+            document.getElementById("flowTextContainer-Title").style.width = (titleWidth * 2) + "px";
+            document.getElementById("flowText-H1-Title").style.width = (titleWidth * 2) + "px";
+            document.getElementById("flowTextContainer-Title").style.animation = "marquee 15s linear infinite";
+
+            // artist에서 animation등 제거
+            document.getElementById("Music-Artist").style.paddingRight = "0";
+            document.getElementById("flowTextContainer-Artist").style.width = "auto";
+            document.getElementById("flowText-H1-Artist").style.width = "auto";
+            document.getElementById("flowTextContainer-Artist").style.animation = "";
+        } else if (artistOverflow) {
+            // artist만 overflow
+            document.getElementById("Music-Artist").style.paddingRight = "80px";
+            artistWidth = document.getElementById("Music-Artist").offsetWidth;
+            document.getElementById("flowTextContainer-Artist").style.width = (artistWidth * 2) + "px";
+            document.getElementById("flowText-H1-Artist").style.width = (artistWidth * 2) + "px";
+            document.getElementById("flowTextContainer-Artist").style.animation = "marquee 15s linear infinite";
+
+            // title에서 animation등 제거
+            document.getElementById("Music-Title").style.paddingRight = "0";
+            document.getElementById("flowTextContainer-Title").style.width = "auto";
+            document.getElementById("flowText-H1-Title").style.width = "auto";
+            document.getElementById("flowTextContainer-Title").style.animation = "";
+        } else {
+            // artist에서 animation등 제거
+            document.getElementById("Music-Artist").style.paddingRight = "0";
+            document.getElementById("flowTextContainer-Artist").style.width = "auto";
+            document.getElementById("flowText-H1-Artist").style.width = "auto";
+            document.getElementById("flowTextContainer-Artist").style.animation = "";
+
+            // title에서 animation등 제거
+            document.getElementById("Music-Title").style.paddingRight = "0";
+            document.getElementById("flowTextContainer-Title").style.width = "auto";
+            document.getElementById("flowText-H1-Title").style.width = "auto";
+            document.getElementById("flowTextContainer-Title").style.animation = "";
+        }
+
+        // 설정을 완료했음을 설정
+        this.setState({setOverFlow: true});
+
+    }
+
+    componentDidUpdate() {
+        if (!this.state.setOverFlow) {
+            this.setFlow();
+        }
+    }
+
+    componentDidMount() {
+        if (!this.state.setOverFlow) {
+            this.setFlow();
+        }
+    }
+
+    render() {
+        const singleTitle = (
+            <span className="flowText" id="Music-Title">{this.state.title}</span>
+        );
+        const singleArtist = (
+            <span className="flowText" id="Music-Artist">{this.state.artist}</span>
+        )
+        return (
+            <div className="MusicPlay-Div">
+                <h1 className="flowText-H1" id="flowText-H1-Title">
+                    <div className="flowTextContainer" id="flowTextContainer-Title">
+                        {singleTitle}
+                        {this.state.overflow.title
+                            ? singleTitle
+                            : ""}
+                    </div>
+                </h1>
+                <h1 className="flowText-H1" id="flowText-H1-Artist">
+                    <div className="flowTextContainer" id="flowTextContainer-Artist">
+                        {singleArtist}
+                        {this.state.overflow.artist
+                            ? singleArtist
+                            : ""}
+                    </div>
+                </h1>
+                <Progress/>
+            </div>
+        );
+    }
+}
+MusicPlay.propTypes = propTypes;
+MusicPlay.defaultProps = defaultProps;
+export default MusicPlay;
---
+++ src/components/Progress.js
@@ -0,0 +1,28 @@
+import React, {Component, PropTypes} from 'react';
+const propTypes = {
+    elapsed: React.PropTypes.number,
+    total: React.PropTypes.number,
+    position: React.PropTypes.number
+};
+const defaultProps = {
+    elapsed: 50,
+    total: 100,
+    position: 0.5
+};
+class Progress extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="progress">
+                <span className="player__time-elapsed">{this.props.elapsed}</span>
+                <progress value={this.props.position} max="1"></progress>
+                <span className="player__time-total">{this.props.total}</span>
+            </div>
+        );
+    }
+}
+Progress.propTypes = propTypes;
+Progress.defaultProps = defaultProps;
+export default Progress;
